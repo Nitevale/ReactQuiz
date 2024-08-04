9e8e18724f5be15257ad307f3811b105db9d7526
Admin <snmvaliente@gmail.com>
2024-08-05 01:36:17 +0800

    Finished Login

--- BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
+++ BE/obj/Debug/net8.0/BE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b39f9e121c695223397fc1afbd9c6b80a9f0bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c8f723a266a905525108e72bc7edf3ffb6da119")]
 [assembly: System.Reflection.AssemblyProductAttribute("BE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
